63257a175ce276ebb1f66002d29dd5c2a9c65b07
Jens Rasmussen <jensr2019@gmail.com>
2022-12-10 19:10:14 -0600

    Day06 complete

--- Day06/fsharp/Program.fs
+++ Day06/fsharp/Program.fs
@@ -1,2 +1,20 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+open System.IO
+
+let part1 (input:String) =
+    input
+        |> Seq.windowed(4)
+        |> Seq.map Set
+        |> Seq.findIndex(fun x -> x.Count = 4)
+        |> (+) 4
+
+let part2 input =
+    input
+        |> Seq.windowed(14)
+        |> Seq.map Set
+        |> Seq.findIndex(fun x -> x.Count = 14)
+        |> (+) 14
+
+let input = File.ReadLines "input.txt"
+part1 (Seq.exactlyOne input) |> Console.WriteLine
+part2 (Seq.exactlyOne input) |> Console.WriteLine
