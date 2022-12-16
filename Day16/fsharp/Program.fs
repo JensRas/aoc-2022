7f2d33ebd1f6b8cce86c933f65d3beea826e3c23
Jens Rasmussen <jensr2019@gmail.com>
2022-12-15 18:36:22 -0600

    Boilerplate

--- Day16/fsharp/Program.fs
+++ Day16/fsharp/Program.fs
@@ -1,2 +1,13 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+open System.IO
+
+let input = File.ReadLines "input.txt"
+
+let part1 input =
+    0
+
+let part2 input =
+    0
+
+part1 input |> Console.WriteLine
+part2 input |> Console.WriteLine
